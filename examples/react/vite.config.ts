f8c5595504eb751f4b125fc75bd2a7f9704bf0db
Mike Bybee <me@mikebybee.com>
2023-02-05 00:02:01 -0600

    Update ./examples/react/vite.config.ts

--- examples/react/vite.config.ts
+++ examples/react/vite.config.ts
@@ -1,6 +1,6 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import { reactRouterPlugin } from 'vite-plugin-screens';
+import { reactRouterPlugin } from '@onerouter/vite-plugin-screens';
 
 // https://vitejs.dev/config/
 export default defineConfig({
